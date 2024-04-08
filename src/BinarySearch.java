8b79ddd98d0fd08a01038b5e61e823e4b048e5aa
shiva <shivatejareddy3008@gmail.com>
2024-04-09 00:08:26 +0530

    second commit

---
+++ src/BinarySearch.java
@@ -0,0 +1,29 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5,6,7,8,9,10};
+        boolean flag= false;
+        int key=8;
+        int l=0;
+        int h=a.length-1;
+
+
+        while(l<=h){
+            int m=(l+h)/2;
+            if(a[m]==key){
+                System.out.println("element found");
+                flag=true;
+                break;
+            }
+            if(a[m]<key){
+                l=m+1;
+
+            }
+            if(a[m]>key){
+                h=m-1;
+            }
+        }
+        if(flag==false){
+            System.out.println("element not found");
+        }
+    }
+}
